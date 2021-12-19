7f2d85173add369dbb066fc9297f9423569fbf1b
gbudau <54190400+gbudau@users.noreply.github.com>
2021-12-19 18:45:13 +0100

    Update challenge 9 - Collisions
    Optimized a bit
    TODO: implement collision mask

--- Chall9_collisions/collisions.py
+++ Chall9_collisions/collisions.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+#from bitarray import bitarray
 import sys
 
 
 class Bitmask:
-    def __init__(self, bitarray, width, height):
-        self.bitarray = bitarray
+    def __init__(self, mask, width, height):
+        self.mask = mask
         self.width = width
         self.height = height
 
@@ -17,25 +18,35 @@ class Sprite:
         self.y = y
         self.xmax = x + bitmask.width
         self.ymax = y + bitmask.height
-        self.pixels = set()
-        self.__init_pixels()
-
-    
-    def __init_pixels(self):
-        for y in range(self.bitmask.height):
-            for x in range(self.bitmask.width):
-                if self.bitmask.bitarray[y * self.bitmask.width + x] == 1:
-                    self.pixels.add((self.x + x, self.y + y))
 
 
     def collides(self, other):
-        return self.__intersects(other) and \
-                any(p in self.pixels for p in other.pixels)
+        # this sprite bottom right co-ordinates
+        self_x1 = self.x + self.bitmask.width - 1
+        self_y1 = self.y + self.bitmask.height - 1
 
+        # other sprite bottom right co-ordinates
+        other_x1 = other.x + other.bitmask.width - 1
+        other_y1 = other.y + other.bitmask.height - 1
 
-    def __intersects(self, other):
-        return self.xmax >= other.x and self.ymax >= other.y and \
-                other.xmax >= self.x and other.ymax >= self.y
+        # check if bounding boxes intersect
+        if other_x1 < self.x or self_x1 < other.x:
+            return False
+        if other_y1 < self.y or self_y1 < other.y:
+            return False
+
+        inter_x0 = max(self.x, other.x)
+        inter_x1 = min(self_x1, other_x1)
+
+        inter_y0 = max(self.y, other.y)
+        inter_y1 = min(self_y1, other_y1)
+
+        for y in range(inter_y0, inter_y1 + 1):
+            for x in range(inter_x0, inter_x1 + 1):
+                if self.bitmask.mask[y - self.y][x - self.x] == 1 and \
+                        other.bitmask.mask[y - other.y][x - other.x] == 1:
+                    return True
+        return False
 
 
 def collisions(sprites):
@@ -54,10 +65,11 @@ def main():
     n = int(sys.stdin.readline().rstrip())
     for i in range(n):
         (width, height) = [int(s) for s in sys.stdin.readline().split()]
-        bitarray = []
+        mask = []
         for j in range(height):
-            bitarray += list(map(int, (sys.stdin.readline().rstrip())))
-        bitmasks.append(Bitmask(bitarray, width, height))
+            #mask.append(bitarray(sys.stdin.readline().rstrip()))
+            mask.append(list(map(int, sys.stdin.readline().rstrip())))
+        bitmasks.append(Bitmask(mask, width, height))
 
     for i in range(cases):
         sprites = []
